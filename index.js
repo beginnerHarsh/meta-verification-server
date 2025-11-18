16432244c3b6228b9d7343e4332ea037161acd57
beginnerHarsh <114108160+beginnerHarsh@users.noreply.github.com>
2025-11-18 22:21:25 +0530

    Initialize Express server with webhook endpoint

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+
+const VERIFY_TOKEN = "verify_token_123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verified!");
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+app.get("/", (req, res) => res.send("Server Running"));
+
+app.listen(3000, () => console.log("Server running on port 3000"));
